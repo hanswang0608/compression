18abdd9ec9144fc195198bdfc99e57346b871554
Hans Wang <hanswang0168@gmail.com>
2021-11-08 15:44:58 -0500

    include std::unordered_map to use as hashtable

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "Tree.h"
 #include "Huffman.h"
-
-void func(){
-    Tree * t2 = new Tree(new Node(5));
-}
+#include <map>
+#include <unordered_map>
 
 int main(int argc, char** argv){
-    Tree t = Tree(new Node(5, new Node(3), new Node(4)));
-    t.print_tree();
-    func();
-    Huffman::compress("testing new2");
+    // Hashtable that stores the frequency of each character read from the input
+    std::unordered_map<char, int> freq_table;
+
+    // Hashtable that stores the variable length huffman codes for each character
+    std::unordered_map<char, std::string> codes_table;
 }
